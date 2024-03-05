0e51f1968a7173aa7ed5ab931e4cce9305dafb69
JoséFranciscoSesé <sesejose@gmail.com>
2024-03-05 14:16:19 +0100

    Fixing build errors

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,17 +3,21 @@
 
 export default async function Posts() {
 
-    const response = await fetch('https://exd.dk/wp-json/wp/v2/posts');
-    const articles = await response.json();
-    console.log("articles");
+    // const response = await fetch('https://exd.dk/wp-json/wp/v2/posts');
+    // const articles = await response.json();
+    // console.log("articles");
 
-    const searchParams = useSearchParams();
-  const id = searchParams.get("id");
+    // const searchParams = useSearchParams();
+    // const id = searchParams.get("id");
 
-    const res = await fetch('https://exd.dk/wp-json/wp/v2/posts') + id + "?_embed";
-    const article = await res.json();
-    const articleSelected = await res.json(); // After getting the data convert it to json.
-    console.log("article");
+    // const res = await fetch('https://exd.dk/wp-json/wp/v2/posts') + id + "?_embed";
+    // const article = await res.json();
+    // const articleSelected = await res.json(); // After getting the data convert it to json.
+    // console.log("article");
    
-    return ({ articles}, {articleSelected })
+    // return ({ articles}, {articleSelected })
+    return (
+      <>
+      </>
+    )
   }
--- src/app/config.js
+++ src/app/config.js
@@ -1,1 +1,2 @@
-export const reqUrl = 'https://exd.dk/wp-json/wp/v2';
+// export const reqUrl = 'https://exd.dk/wp-json/wp/v2';
+export const reqUrl = 'https://entremasas.grapixmo.com/wp-json/wp/v2/';
