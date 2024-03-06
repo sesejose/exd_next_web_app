5ff48389c57ac39379ab311c9fb7afa581db6011
JoséFranciscoSesé <sesejose@gmail.com>
2024-03-06 11:57:29 +0100

    Fixing build error fetch when Deploy

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,2 @@
-// export const reqUrl = 'https://exd.dk/wp-json/wp/v2';
-export const reqUrl = 'https://https://exd.grapixmo.com/wp-json/wp/v2/posts';
+export const reqUrl = 'https://exd.dk/wp-json/wp/v2';
+// export const reqUrl = 'https://https://exd.grapixmo.com/wp-json/wp/v2/posts';
