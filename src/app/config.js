ff97ccd97ac0cab5db58e5bbe85c65c12543dd68
JoséFranciscoSesé <sesejose@gmail.com>
2024-03-13 23:07:57 +0100

    &per_page=100

--- src/app/config.js
+++ src/app/config.js
@@ -2,5 +2,5 @@
 // export const reqUrl = 'https://public-api.wordpress.com/rest/v1.1/sites/exdinsights.wordpress.com/';
 // export const reqUrl = 'https://exd.grapixmo.com/wp-json/wp/v2';
 // export const reqUrl = 'https://exd.grapixmo.com/wp-json/wp/v2/article?_embed';
-export const reqUrl = 'https://exd-insight.dk/wp-json/wp/v2/article?_embed';
+export const reqUrl = 'https://exd-insight.dk/wp-json/wp/v2/article?_embed&per_page=100';
 // export const catUrl = 'https://exd.grapixmo.com/wp-json/wp/v2/categories/';
