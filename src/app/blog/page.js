5899c1aba6c8b8ef1c480c13478041867750d2ea
JoséFranciscoSesé <sesejose@gmail.com>
2024-03-15 10:11:49 +0100

    just deploy

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,31 +1,42 @@
-// 'use client'
+'use client'
 
 import Article from "../components/Article";
 import { reqUrl } from "@/app/config";
 import InsightsHero from "../components/InsightsHero";
+import { useState } from "react";
+import { useEffect } from "react";
 
-async function getData() {
-  const res = await fetch('https://exd-insight.dk/wp-json/wp/v2/article?_embed&per_page=100')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function getData() {
+//   const res = await fetch('https://exd-insight.dk/wp-json/wp/v2/article?_embed&per_page=100')
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
  
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
  
-  return res.json()
-}
+//   return res.json()
+// }
 
 // I fetch the articles and pass the them as props to and through the Article component tag 
 
 export default async function Insights() {
 
-  // const response = await fetch(`${reqUrl}`); 
-  // const articles = await response.json();
-  // console.log(articles);
+  const response = await fetch(`${reqUrl}`); 
+  const articles = await response.json();
+  console.log(articles);
 
-  const articles = await getData()
+  // const articles = await getData()
+  
+
+  // const [id, setId] = useState();
+  // const [category, setCategory] = useState();
+  // const [image, setImage] = useState();
+  // const [title, setTitle] = useState();
+  // const [excerpt, setExcrpt] = useState();
+  // const [content, setContent] = useState();
+  // const [slug, setSlug] = useState();
 
   return (
     <>
