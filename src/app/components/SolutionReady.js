9725861ff7461483e4decabacc7344dea4718dac
JoséFranciscoSesé <sesejose@gmail.com>
2024-05-08 11:25:53 +0200

    Texts

--- src/app/components/SolutionReady.js
+++ src/app/components/SolutionReady.js
@@ -9,8 +9,8 @@ export default function SolutionReady() {
             <div id="solution-ready">
                 <div className="grid flex-column-center gap-2">
                     <div className="col-3-11">
-                        <h4 className="ptb">At EXD Experience Design, we redefine workplace experiences with innovative solutions. Our Digital Platforms, OutSystems and 4me, are the pillars of this transformation. </h4>
-                        <div className="text-1 ptb">Discover how OutSystem revolutionizes mobile UX, while 4me simplifies service management. Dive into the future of workplace efficiency.
+                        <h4 className="ptb">At EXD Experience Design, we redefine workplace experiences with innovative solutions. Our Digital Platforms, 4me and Autom Mate, are the pillars of this transformation.</h4>
+                        <div className="text-1 ptb">Explore the power of 4me in streamlining service management and discover how Autom Mate boosts productivity and efficiency. Step into the future of workplace optimization.
                         <br></br>Ready to Elevate?</div>
                         <Link href={"/products"} className="btn-purple">
                             <div className="text-2">Learn more</div>
