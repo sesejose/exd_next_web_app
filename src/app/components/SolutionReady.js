8061cf9653aa0ce5f247d6e83062d6d3ca101aaa
JoséFranciscoSesé <sesejose@gmail.com>
2024-05-08 15:39:41 +0200

    Texts & Links

--- src/app/components/SolutionReady.js
+++ src/app/components/SolutionReady.js
@@ -9,7 +9,7 @@ export default function SolutionReady() {
             <div id="solution-ready">
                 <div className="grid flex-column-center gap-2">
                     <div className="col-3-11">
-                        <h4 className="ptb">At EXD Experience Design, we redefine workplace experiences with innovative solutions. Our Digital Platforms, 4me and Autom Mate, are the pillars of this transformation.</h4>
+                        <h4 className="ptb">At Experience Design, we redefine workplace experiences with innovative solutions. Our Digital Platforms, 4me and Autom Mate, are the pillars of this transformation.</h4>
                         <div className="text-1 ptb">Explore the power of 4me in streamlining service management and discover how Autom Mate boosts productivity and efficiency. Step into the future of workplace optimization.
                         <br></br>Ready to Elevate?</div>
                         <Link href={"/products"} className="btn-purple">
