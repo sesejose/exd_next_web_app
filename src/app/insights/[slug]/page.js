fe0bf33a1cac560341e9f19dbebfd18b0681a2e1
JoséFranciscoSesé <sesejose@gmail.com>
2024-05-23 14:39:04 +0200

    renderParagraphs

--- src/app/insights/[slug]/page.js
+++ src/app/insights/[slug]/page.js
@@ -5,11 +5,10 @@ import { Link } from 'next-view-transitions'
 // import Link from "next/link";
 // import ArticleOthers from "@/app/components/ArticlesOthers";
 
-// It receive the {params} from the component Article.js dynamic URL
-// If I console.log the params => I'll see that this is the slug defined in the link of the component
+// It receives the {params} from the component Article.js dynamic URL
+// If you console.log the params => you'll see that this is the slug defined in the link of the component
 // It uses those { params } to fetch the article clicked from the list at insights/page.js
 // Then it defines the variable { post } to set up the elements for the details in the return
-
 
 // With this particular function I'm fetching the list of Articles
 async function getArticles(data) {
@@ -17,11 +16,9 @@ async function getArticles(data) {
     // const data = await res.json();
     return res.json();
     // console.log('Articles', data);
+}
 
-  }
-
-export default async function Post( { params } ) {
-
+export default async function Post({ params }) {
     console.log('PAAAAAAAAAAAAAAAAAAAAAAARAMS', params);
 
     // const articlesData = getArticles(data);
@@ -40,44 +37,41 @@ export default async function Post( { params } ) {
     // const imgageBg = post["_embedded"]["wp:featuredmedia"]["0"].link;
     const imgageBg = post.image_article.rendered;
 
+    // Function to render paragraphs
+    const renderParagraphs = (paragraphs) => {
+        return paragraphs.map((paragraph, index) => (
+            <div key={index} className="pb">{paragraph}</div>
+        ));
+    };
+
     return (
         <>
             <div className="wide">
-                <div className="col-12 article-main-image" style={{backgroundImage: `url(${imgageBg})`}}>  
-                <div className="overlay">
-                    <div className="flex-column-center">
-                        <p className="zero bold">Created: {post.date}</p>
-                        <p className="post-category bold">{post["_embedded"]["wp:term"]["0"]["0"].name}</p>
-                    </div>
-
-                </div>
-                </div>
-                
-                <div id="article_opened">
-                    {/* <div className="grid-center">
-                        <div className="col-12" > 
-                        className="article-main-image" style={{backgroundImage: `url(${imgageBg})`}}        
+                <div className="col-12 article-main-image" style={{ backgroundImage: `url(${imgageBg})` }}>
+                    <div className="overlay">
+                        <div className="flex-column-center">
+                            <p className="zero bold">Created: {post.date}</p>
+                            <p className="post-category bold">{post["_embedded"]["wp:term"]["0"]["0"].name}</p>
                         </div>
-                    </div> */}
-                    
+                    </div>
+                </div>
+
+                <div id="article_opened">
                     <div className="grid-center text-left padding">
                         <div className="col-3-11 flex-column-center pb-large">
                             <h2 className="w-100 title-article-opened">{post.title_article.rendered}</h2>
-                            <Image src={post.body_image_article.rendered} width={160} height={90} alt="Image" className="post-img-body"></Image>
+                            <Image src={post.body_image_article.rendered} width={160} height={90} alt="Image" className="post-img-body" />
                             <div className="span bold purple-light">{post.body_caption_article.rendered}</div>
                             <h3 className="w-100 ptb-medium">{post.body_headline_article.rendered}</h3>
-                            {/* <div className="text-1">{post.body_text_article.rendered}</div> */}
                             <div className="w-100 text-1">
-                                <div className="pb">{post.body_text_article.value[0]}</div>
-                                <div className="pb">{post.body_text_article.value[1]}</div>
-                                <div className="pb">{post.body_text_article.value[2]}</div>
+                                {renderParagraphs(post.body_text_article.value)}
                             </div>
                             <div className="article-quote p-mobile-medium text-center">{post.body_quote_article.rendered}</div>
                             <div className="w-100 text-1">{post.body_more_text_article.rendered}</div>
                             <div className="w-100 bold ptb">{post.body_link_text_article.rendered}</div>
                             <Link href={`${post.body_link_article.rendered}`} className="w-100 text-link-purple">{post.body_link_article.rendered}</Link>
                             <div className="w-100 bold ptb">{post.body_email_text_article.rendered}</div>
-                            <Link href={`${post.body_email_article.rendered}`} className="w-100 text-link-purple">{post.body_email_article.rendered}</Link>
+                            <Link href={`mailto:${post.body_email_article.rendered}`} className="w-100 text-link-purple">{post.body_email_article.rendered}</Link>
                         </div>
                     </div>
                 </div>
