782fd6b30710df410b058ff9068f165bc705dec4
JoséFranciscoSesé <sesejose@gmail.com>
2024-05-25 10:01:43 +0200

    If Image Body Article Render It

--- src/app/insights/[slug]/page.js
+++ src/app/insights/[slug]/page.js
@@ -44,6 +44,19 @@ export default async function Post({ params }) {
         ));
     };
 
+     // Function to conditionally render the image
+     const renderImage = (imageUrl) => {
+        if (imageUrl) {
+            return (
+                <div className="image-container">
+                    <Image src={imageUrl} width={160} height={90} alt="Article Image" className="post-img-body" />
+                </div>
+            );
+        }
+        return null;
+    };
+
+
     return (
         <>
             <div className="wide">
@@ -60,7 +73,8 @@ export default async function Post({ params }) {
                     <div className="grid-center text-left padding">
                         <div className="col-3-11 flex-column-center pb-large">
                             <h2 className="w-100 title-article-opened">{post.title_article.rendered}</h2>
-                            <Image src={post.body_image_article.rendered} width={160} height={90} alt="Image" className="post-img-body" />
+                            {/* <Image src={post.body_image_article.rendered} width={160} height={90} alt="Image" className="post-img-body" /> */}
+                            {renderImage(post.body_image_article.rendered)}
                             <div className="span bold purple-light">{post.body_caption_article.rendered}</div>
                             <h3 className="w-100 ptb-medium">{post.body_headline_article.rendered}</h3>
                             <div className="w-100 text-1">
