117fa4ca4294265b19ab82deaf75120456522046
JoséFranciscoSesé <sesejose@gmail.com>
2024-05-29 18:48:07 +0200

    Excerpts

--- src/app/components/FourmeIntro.js
+++ src/app/components/FourmeIntro.js
@@ -12,21 +12,20 @@ export default function FourmeIntro() {
                 <p className="counter bold">4me Specialist view</p>
             </div> */}
 
-            <section className="flex-column-center">
-                <video id="fourme-intro" width="320" height="171" controls preload="none" loop muted className="fourme-intro">
-                    <source src="/fourme_self_service_desktop.mp4" type="video/mp4" />
-                    {/* <track
-                        src="/path/to/captions.vtt"
-                        kind="subtitles"
-                        srcLang="en"
-                        label="English"
-                    /> */}
-                    <p>Your browser does not support the video tag.</p>
-                </video>
+            <section className="flex-column-center w-100">
+                <div className="video-container">
+                    <video id="fourme-intro" poster="/poster.jpg" controls preload="none" loop muted className="fourme-intro">
+                        <source src="/fourme_self_service_desktop.mp4" type="video/mp4" />
+                        <p>Your browser does not support the video tag.</p>
+                    </video>
+                </div>
                 <p className="counter bold">4me Specialist & End-user view</p>
             </section>
 
         </div>
         </>
-    );S
+    );
 }
+
+
+// width="320" height="171" 
